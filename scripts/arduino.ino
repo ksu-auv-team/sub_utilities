cfd33c910be9a2a5c44160e426fee6c7d5bb9417
Tyler Gragg <jayson.gragg@gtri.gatech.edu>
2019-07-16 13:12:19 -0400

    Changed arduino script to use ros

--- scripts/arduino.ino
+++ scripts/arduino.ino
@@ -1,13 +1,23 @@
+#include <ros.h>
+#include <std_msgs/Bool.h>
+
+ros::NodeHandle nh;
+
+std_msgs::Bool is_killed;
+ros::Publisher killswitch_pub("killswitch_status", &is_killed);
+
 void setup() {
-  Serial.begin(9600);
-  pinMode(4, INPUT);
+    ng.initNode();
+    nh.advertise(killswitch_pub);
 }
 
 void loop() {
-  if(digitalRead(4)){
-    Serial.write("1");
-  }
-  else{
-    Serial.write("0");
-  }
+    if(digitalRead(4)){
+        is_killed.data = false;
+        killswitch_pub.publish(&is_killed);
+    }
+    else{
+        is_killed.data = true;
+        killswitch_pub.publish(&is_killed);
+    }
 }
