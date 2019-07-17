f6a529a78028b7e8f68fd1cf38712ee1d967edad
Tyler Gragg <jayson.gragg@gtri.gatech.edu>
2019-07-17 13:26:38 -0400

    Separated runsub into two commands. Startup and runtime, with extra commands

--- scripts/runsub.py
+++ scripts/runsub.py
@@ -15,12 +15,7 @@ class SubSession():
     def __init__(self, no_arduino=False):
         # Subprocesses:
         self.curr_children = []
-        self.arduino_process = []
-        self.arduino = None
-        self.rc = None
-        self.network = None
-        self.mv = None
-        self.ex = None
+        self.startup_processes = []
         
         # Arduino variables
         self.delay_start = 0
@@ -36,7 +31,7 @@ class SubSession():
     # shut down child processes for restarting them cleanly or exiting
     def kill_children(self):
         self.curr_children
-        print("Removing all processes...")
+        print("Removing all runtime processes...")
         for i in range(len(self.curr_children)):
             try:
                 proc = self.curr_children.pop()
@@ -46,22 +41,24 @@ class SubSession():
                     proc.kill()
             except Exception as e:
                 print(e)
+        '''
         bashCommand = "pkill -f ros"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE) 
+        '''
         print("Done!")
 
-    def kill_arduino(self):
-        print("Removing Arduino Process...")
-        for i in range(len(self.arduino_process)):
+    def kill_startup(self):
+        print("Removing Startup Process...")
+        for i in range(len(self.startup_processes)):
             try:
-                proc = self.arduino_process.pop()
+                proc = self.startup_processes.pop()
                 print("Killing: ")
                 print(proc)
                 if not proc.poll():
                     proc.kill()
             except Exception as e:
                 print(e)
-            print("Removed Arduino Process!")
+            print("Removed Startup Process!")
 
     # listens to the killswitch over serial for state changes and calls start() and kill_children() when necessary
     def listen(self):
@@ -77,29 +74,83 @@ class SubSession():
                 self.kill_children()
                 print('stopped')
 
-    #start ALL the things
-    def start(self):
-        # Create commands to run from argparse
+    def start_roscore(self):
         roscore_command = ['roscore']
 
+        print('starting roscore')
+        with open('{}roscoreout.txt'.format(self.curr_log_dir), 'w') as rcout:
+            rc = subprocess.Popen(roscore_command, stdout=rcout, stderr=rcout)
+            return rc
+
+    def start_video(self):
         video_string = "python " + self.script_directory + "pict.py " + args.no_save_images
         video_command = video_string.split()
 
+        print("starting video node")
+        with open('{}videoout.txt'.format(self.curr_log_dir), 'w') as videoout:
+            video = subprocess.Popen(video_command, stdout=videoout, stderr=videoout)
+            return video
+
+    def start_network(self):
         network_string = "python3 " + self.script_directory + '../submodules/jetson_nano_inference/jetson_live_object_detection.py --no-video --model ' + args.network_model + ' ' + args.no_save_images
         network_command = network_string.split()
+    
+        print('starting Neural Network')
+        with open('{}networkout.txt'.format(self.curr_log_dir), 'w') as networkout:
+            network = subprocess.Popen(network_command, stdout=networkout, stderr=networkout)
+            return network
 
+    def start_movement(self):
         movement_string = "roslaunch movement_package manualmode.launch"
         movement_command = movement_string.split()
 
+        print('starting movement_package')
+        with open('{}movementout.txt'.format(self.curr_log_dir), 'w') as mvout:
+            mv = subprocess.Popen(movement_command, stdout=mvout, stderr=mvout)
+            return mv 
+
+    def start_execute(self):
         execute_string = 'python ' + self.script_directory + '../submodules/subdriver2018/execute_withState.py --machine ' + args.state_machine + ' ' + args.debug_execute
         execute_command = execute_string.split()
 
-        # Start running actual commands:
-        print('starting roscore')
-        with open('{}roscoreout.txt'.format(self.curr_log_dir), 'w') as rcout:
-            self.rc = subprocess.Popen(roscore_command, stdout=rcout, stderr=rcout)
-        self.curr_children.append(self.rc)
-           
+        print('starting execute')
+        with open('{}executeout.txt'.format(self.curr_log_dir), 'w') as executeout:
+            ex = subprocess.Popen(execute_command, stdout=executeout, stderr=executeout)
+            return ex
+                
+    def start_arduino(self):
+        arduino_string = "rosrun rosserial_python serial_node.py /dev/arduino_0"
+        arduino_command = arduino_string.split()
+
+        print('starting Arduino Process')
+        with open('{}arduinoout.txt'.format(self.curr_log_dir), 'w') as ardout:
+            arduino = subprocess.Popen(arduino_command, stdout=ardout, stderr=ardout)
+            return arduino
+
+    #start ALL the things
+    def start(self):     
+        
+        # Run the Video Node
+        self.curr_children.append(self.start_video())
+        
+        self.delay_start = time.time() # The time we will compare our arduino time to
+        while(time.time() - self.delay_start < 10):
+            pass
+
+        # Run Movement Package
+        self.curr_children.append(self.start_movement())
+
+        self.delay_start = time.time() # The time we will compare our arduino time to
+        while(time.time() - self.delay_start < 10):
+            pass
+
+        # Run Execute
+        if(args.manual):
+            print('Manual Mode enabled, start your joystick node')
+        else:
+            self.curr_children.append(self.start_execute())
+
+        '''
         self.delay_start = time.time() # The time we will compare our arduino time to
 
         # Run Video / Network Commands:
@@ -109,15 +160,9 @@ class SubSession():
         else:
             time.sleep(10)
         if (args.no_network and run_video):
-            print("starting video node")
-            with open('{}videoout.txt'.format(self.curr_log_dir), 'w') as videoout:
-                self.video = subprocess.Popen(video_command, stdout=videoout, stderr=videoout)
-            self.curr_children.append(self.video)
+            self.curr_children.append(self.start_video())
         elif (run_video):
-            print('starting Neural Network')
-            with open('{}networkout.txt'.format(self.curr_log_dir), 'w') as networkout:
-                self.network = subprocess.Popen(network_command, stdout=networkout, stderr=networkout)
-            self.curr_children.append(self.network)
+            self.curr_children.append(self.start_network())
         else:
             return
 
@@ -129,11 +174,8 @@ class SubSession():
             run_movement = not self.delay_read(10)
         else:
             time.sleep(10)
-        if (run_movement):
-            print('starting movement_package')
-            with open('{}movementout.txt'.format(self.curr_log_dir), 'w') as mvout:
-                self.mv = subprocess.Popen(movement_command, stdout=mvout, stderr=mvout)    
-            self.curr_children.append(self.mv)
+        if (run_movement):   
+            self.curr_children.append(self.start_movement())
         else:
             return
         
@@ -147,14 +189,12 @@ class SubSession():
         else:
             time.sleep(20)
         if (not args.manual and run_execute):
-            print('starting execute')
-            with open('{}executeout.txt'.format(self.curr_log_dir), 'w') as executeout:
-                self.ex = subprocess.Popen(execute_command, stdout=executeout, stderr=executeout)
-            self.curr_children.append(self.ex)
+            self.curr_children.append(self.start_execute())
         elif (args.manual):
             print('Manual Mode')
         else:
             return
+        '''
                 
         print('exiting start')
 
@@ -178,7 +218,7 @@ class SubSession():
         print("\nCaptured Ctrl+C, stopping execution...")
         self.kill_children()
         if not self.no_arduino:
-            self.kill_arduino()
+            self.kill_startup()
         sys.exit(0)
 
     def killswitch_callback(self, msg):
@@ -187,7 +227,7 @@ class SubSession():
             self.sub_is_killed = True
             self.kill_children()
         elif (not msg.data and self.sub_is_killed==True):
-            print('Starting Sub')
+            print('Starting Sub Runtime Processes')
             self.sub_is_killed = False
             self.start()
 
@@ -217,16 +257,15 @@ if __name__ == '__main__':
     
     # If we are running without an arduino hooked up, just run the start, don't listen()
     if args.no_arduino:
+        go_sub_go.startup_processes.append(go_sub_go.start_roscore())
         go_sub_go.start()
 
     # If we do have an arduino hooked up, we need to forward the ROS stuff over
     else:
-        print('starting Arduino Process')
-        arduino_string = "rosrun rosserial_python serial_node.py /dev/arduino_0"
-        arduino_command = arduino_string.split()
-        with open('{}arduinoout.txt'.format(go_sub_go.curr_log_dir), 'w') as ardout:
-            go_sub_go.arduino = subprocess.Popen(arduino_command, stdout=ardout, stderr=ardout)
-        go_sub_go.arduino_process.append(go_sub_go.arduino)
+        go_sub_go.startup_processes.append(go_sub_go.start_roscore())
+        go_sub_go.startup_processes.append(go_sub_go.start_arduino())
+        if(not args.no_network):
+            go_sub_go.startup_processes.append(go_sub_go.start_network())
 
     #the loop everything runs from
     rospy.spin()
