037c65f5ca30aab7d642cc46bbe0e6683e4f6c0e
Tyler Gragg <robots@tylergragg.com>
2019-07-24 21:38:29 -0400

    Can now specify video port and camera pixels

--- scripts/pict.py
+++ scripts/pict.py
@@ -8,9 +8,11 @@ import datetime
 import argparse
 
 def main():
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(args.camera)
+    cam.set(3, args.width)
+    cam.set(4, args.height)
+
     bridge = cv_bridge.CvBridge()
-    #cv2.startWindowThread()
 
     if (not args.debug):
         rospy.init_node('Sub_Video_Publish')
@@ -62,6 +64,9 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--no-save-images', action='store_true', help="Boolean flag to not save images to 'saved_video'")
     parser.add_argument('-d', '--debug', action='store_true', help='Wll not run the ros stuff, allows for just running the video')
     parser.add_argument('-v', '--show-video', action='store_true', help='Will show the video onboard with opencv')
+    parser.add_argument('-c', '--camera', default='/dev/video0', help='/path/to/video, defaults to /dev/video0')
+    parser.add_argument('--height', default=420, help='Set the video capture height for your camera in pixels')
+    parser.add_argument('--width', default=860, help='Set the video capture width for your camera in pixels')
     args = parser.parse_args()
 
     main()
