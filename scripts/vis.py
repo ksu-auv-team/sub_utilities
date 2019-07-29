fd451761ce03168745de717098ae29e2a5e97a74
Tyler Gragg <robots@tylergragg.com>
2019-07-29 18:05:56 -0400

    Added starting of network flags, minor health stuff

--- scripts/vis.py
+++ scripts/vis.py
@@ -1,6 +1,7 @@
 import cv2
 import rospy
 from sensor_msgs.msg import Image
+from std_msgs.msg import Bool
 import time 
 import cv_bridge
 import argparse
@@ -30,6 +31,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This script pulls images over ros topic and displays them to the local computer")
     parser.add_argument('-n', '--network', action='store_true', help='Will listen to the network_imgs topic and display images')
     parser.add_argument('-r', '--raw', action='store_true', help='Will listen to the raw_imgs topic and display images')
+    parser.add_argument('-s', '--start-networks', action='store_true', help='Send start message on enable network topic')
     parser.add_argument('--no-front', action='store_true', help='Will not view the front camera')
     parser.add_argument('--no-bottom', action='store_true', help='Will not view the bottom camera')
     args = parser.parse_args()
@@ -51,6 +53,14 @@ if __name__ == "__main__":
         exit()
 
     rospy.init_node('vis_node', anonymous=True)
+
+    if (args.start_networks):
+        front_pub = rospy.Publisher('enable_front_network', Bool, queue_size=1)
+        bottom_pub = rospy.Publisher('enable_bottom_network', Bool, queue_size=1)
+        time.sleep(0.5)
+        front_pub.publish(True)
+        bottom_pub.publish(True)
+
     rospy.spin()
 
 cv2.destroyAllWindows()
