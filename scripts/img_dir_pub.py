0313816583e4ffe8ddbb71f69ded9996e6c98c09
Tyler Gragg <robots@tylergragg.com>
2019-10-25 15:25:22 -0400

    removed commented out code. requires -p argument

--- scripts/img_dir_pub.py
+++ scripts/img_dir_pub.py
@@ -50,22 +50,13 @@ def main():
 
 if __name__ == "__main__":
     # Parse command line arguments:
-    parser = argparse.ArgumentParser(description="Get video from the sub over a ros topic")
-    parser.add_argument('-p', '--img_path', help='Path to the folder of images to read. Required.')
+    parser = argparse.ArgumentParser(description="Publishes images stored in a directory to either the front_raw_imgs or bottom_raw_imgs topic")
+    parser.add_argument('-p', '--img_path', required=True, help='Path to the folder of images to read. Required.')
     parser.add_argument('-v', '--show_video', action='store_true', help='Will show the video onboard with opencv')
     parser.add_argument('-t', '--topic', type=int, default='0', help='int representing topic to publish images to. 0 is front cam, 1 is bottom cam.s')
     parser.add_argument('-f', '--fps', type=int, default='10', help='number of frames per second to publish. Entering 0 with -v will set opencv to continue/publish only on a keypress. Default is 10.')
     parser.add_argument('-s', '--shuffle', action='store_true', help='shuffle images')
-    #disabled for now - may reimplement these later to resize images
-    # parser.add_argument('--front-height', default=420, type=int, help='Set the front video capture height for your camera in pixels')
-    # parser.add_argument('--bottom-height', default=420, type=int, help='Set the bottom video capture height for your camera in pixels')
-    # parser.add_argument('--front-width', default=860, type=int, help='Set the front video capture width for your camera in pixels')
-    # parser.add_argument('--bottom-width', default=860, type=int, help='Set the bottom video capture width for your camera in pixels')
     args = parser.parse_args()
-
-    if not args.img_path:
-        print("Must provide directory to load images from with -p")
-        exit()
 
     if args.fps == 0 and not args.show_video:
         print("Cannot use 0 FPS without -v")
