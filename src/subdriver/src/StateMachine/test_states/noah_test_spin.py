7caeee29d71938625e91cdbeb130ae63a74f04ab
Noah Trinite <trinitenoah@gmail.com>
2020-06-24 14:15:43 -0400

    Added math

--- src/subdriver/src/StateMachine/test_states/noah_test_spin.py
+++ src/subdriver/src/StateMachine/test_states/noah_test_spin.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 
 from StateMachine.sub import *
+import math
+
 
 #defines test state
 class NoahTestSpin(Sub):
@@ -13,18 +15,27 @@ class NoahTestSpin(Sub):
     def execute(self, userdata):
         self.init_state()
 
+        #init_heading = gbl.init_heading
+
         gbl.state_heading = gbl.heading
         msg = self.init_joy_msg()
 
-        msg.axes[const.AXES['rotate']] = -0.5
+        magnitude = 1
+
+        msg.axes[const.AXES['rotate']] = 0.3
 
         rospy.loginfo("just trying to spin")
         while(1):
             self.publish_joy(msg)
 
+            angle = self.angle_diff(gbl.heading, gbl.state_heading)
+            frontback = math.cos(anlge) * magnitude
+            strafe = math.sin(angle) * magnitude
+            msg.axes[const.AXES['frontback']] = frontback
+            msg.axes[const.AXES['strafe']] = strafe
 
-
-        
+            rospy.sleep(const.SLEEP_TIME)
+            
         print("wow")
 
         return 'through_gate'
