907a33a4176b32b9130ff8fa6b142d08fe094369
Noah Trinite <trinitenoah@gmail.com>
2020-06-24 19:56:52 -0400

    It sure does party

--- src/subdriver/src/StateMachine/test_states/noah_test_spin.py
+++ src/subdriver/src/StateMachine/test_states/noah_test_spin.py
@@ -11,28 +11,37 @@ class NoahTestSpin(Sub):
     def __init__(self):
         smach.State.__init__(self, outcomes=['through_gate'])
 
+    def calc_frontback(self, angle):
+        frontback = 0.0
+        frontback = math.sin(angle) * 0.3
+        return frontback
+
+    def calc_strafe(self, angle):
+        strafe = 0.0
+        strafe = math.cos(angle) * 0.3
+        return strafe
     
     def execute(self, userdata):
         self.init_state()
 
-        #init_heading = gbl.init_heading
+        initial_heading = gbl.init_heading
 
         gbl.state_heading = gbl.heading
         msg = self.init_joy_msg()
 
-        magnitude = 1
-
+        magnitude = 0.3
+        frontback = 0.0
+        strafe = 0.0
         msg.axes[const.AXES['rotate']] = 0.3
 
         rospy.loginfo("just trying to spin")
         while(1):
-            self.publish_joy(msg)
-
-            angle = self.angle_diff(gbl.heading, gbl.state_heading)
-            frontback = math.cos(anlge) * magnitude
-            strafe = math.sin(angle) * magnitude
+            angle = self.angle_diff(gbl.init_heading, gbl.heading)
+            frontback = self.calc_frontback(angle)
+            strafe = self.calc_strafe(angle)
             msg.axes[const.AXES['frontback']] = frontback
             msg.axes[const.AXES['strafe']] = strafe
+            self.publish_joy(msg)
 
             rospy.sleep(const.SLEEP_TIME)
             
