48922709f768ee18ede60eedbb2f647c47966a06
Noah Trinite <trinitenoah@gmail.com>
2020-06-24 23:51:59 -0400

    Funfact: math.cos/sin uses RADIANS!

--- src/subdriver/src/StateMachine/test_states/noah_test_spin.py
+++ src/subdriver/src/StateMachine/test_states/noah_test_spin.py
@@ -11,14 +11,14 @@ class NoahTestSpin(Sub):
     def __init__(self):
         smach.State.__init__(self, outcomes=['through_gate'])
 
-    def calc_frontback(self, angle):
+    def calc_frontback(self, angle, magnitude):
         frontback = 0.0
-        frontback = math.sin(angle) * 0.3
+        frontback = math.cos(math.radians(angle)) * magnitude
         return frontback
 
-    def calc_strafe(self, angle):
+    def calc_strafe(self, angle, magnitude):
         strafe = 0.0
-        strafe = math.cos(angle) * 0.3
+        strafe = math.sin(math.radians(angle)) * magnitude
         return strafe
     
     def execute(self, userdata):
@@ -29,23 +29,29 @@ class NoahTestSpin(Sub):
         gbl.state_heading = gbl.heading
         msg = self.init_joy_msg()
 
-        magnitude = 0.3
-        frontback = 0.0
-        strafe = 0.0
-        msg.axes[const.AXES['rotate']] = 0.3
+        #magnitude is the hypot
+        magnitude = .07
+
+        msg.axes[const.AXES['rotate']] = -0.2
 
         rospy.loginfo("just trying to spin")
         while(1):
             #Idk which heading to use RIP
             angle = self.angle_diff(gbl.init_heading, gbl.heading)
-            frontback = self.calc_frontback(angle)
-            strafe = self.calc_strafe(angle)
+            if angle < 0:
+                  angle += 360
+            rospy.loginfo("Angle " + str(angle))
+            frontback = self.calc_frontback(angle, magnitude)
+            strafe = self.calc_strafe(angle, magnitude)
+            rospy.loginfo("FB " + str(frontback))
+            rospy.loginfo("strafe " + str(strafe))
             msg.axes[const.AXES['frontback']] = frontback
             msg.axes[const.AXES['strafe']] = strafe
+            #rospy.loginfo(magnitude)
             self.publish_joy(msg)
 
             rospy.sleep(const.SLEEP_TIME)
             
-        print("wow")
+    
 
         return 'through_gate'
