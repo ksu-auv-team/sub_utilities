f2aaeb6d95071f918879de072b54a4cdd85c3ecd
Noah Trinite <trinitenoah@gmail.com>
2020-06-30 19:04:39 -0400

    here you go dad, Still doesn't work

--- src/subdriver/src/StateMachine/test_states/noah_test_spin.py
+++ src/subdriver/src/StateMachine/test_states/noah_test_spin.py
@@ -13,14 +13,18 @@ class NoahTestSpin(Sub):
 
     def calc_frontback(self, angle, magnitude):
         frontback = 0.0
+        #CAH
         frontback = math.cos(math.radians(angle)) * magnitude
         return frontback
 
     def calc_strafe(self, angle, magnitude):
         strafe = 0.0
+        #SOH
         strafe = math.sin(math.radians(angle)) * magnitude
         return strafe
     
+
+
     def execute(self, userdata):
         self.init_state()
 
@@ -30,19 +34,20 @@ class NoahTestSpin(Sub):
         msg = self.init_joy_msg()
 
         #magnitude is the hypot
-        magnitude = 0.1
+        magnitude = .1
 
-        msg.axes[const.AXES['rotate']] = -0.1
+        msg.axes[const.AXES['rotate']] = -.1
         rospy.loginfo("just trying to spin")
         while(1):
             #Idk which heading to use RIP
             angle = self.angle_diff(gbl.init_heading, gbl.heading)
-            angle2 = self.angle_diff(gbl.init_heading, angle)
-          
+
+
+
             rospy.loginfo("Angle " + str(angle))
-            rospy.loginfo("Radian " + str(math.radians(angle)))
-            frontback = self.calc_frontback(angle2, magnitude)
-            strafe = self.calc_strafe(angle2, magnitude)
+            #rospy.loginfo("Radian " + str(math.radians(angle)))
+            frontback = self.calc_frontback(angle, magnitude)
+            strafe = self.calc_strafe(angle, magnitude)
             rospy.loginfo("FB " + str(frontback))
             rospy.loginfo("strafe " + str(strafe))
             msg.axes[const.AXES['strafe']] = strafe
