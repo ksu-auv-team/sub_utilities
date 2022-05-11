4ca3cb8778f45d831f30966b1e8df841a082ee7f
Noah Trinite <55561667+ntrinite@users.noreply.github.com>
2022-05-10 22:09:59 -0400

    Create strafe_right_test_state.py Not yet tested

---
+++ src/subdriver/src/StateMachine/test_states/strafe_right_test_state.py
@@ -0,0 +1,23 @@
+from StateMachine.sub import *
+
+# Sole purpose is to test the sub's ability to strafe left
+class StrafeRight(Sub):
+  
+  # Gives a list of outcomes from this state
+  def __init__(self):
+		smach.State.__init__(self,outcomes=['strafed_right'])
+		
+	def execute(self, userdata):
+		self.init_state()
+		strafe_magnitude = -.5
+		
+		msg = self.init_joy_msg()
+									
+		while(rospy.get_time() - self.current_state_start_time < 5):
+			rospy.loginfo("strafing right')
+			msg.axes[const.AXES['strafe']] =  strafe_magnitude
+			self.publish(msg)
+			rospy.sleep(const.SLEEP_TIME)
+									
+		rospy.loginfo("We should've strafed right")
+		return 'strafed_right'
