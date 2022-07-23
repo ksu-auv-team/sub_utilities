40bc6dc2a37f5026316f9185f3d32a7fa63fc4b8
Noah Trinite <trinitenoah@gmail.com>
2022-07-23 12:04:08 -0400

    garbo_fix_install maybe

--- src/network_wrapper/jetson_live_object_detection.py
+++ src/network_wrapper/jetson_live_object_detection.py
@@ -19,8 +19,8 @@ class JetsonLiveObjectDetection():
     def __init__(self, model, camera=None, debug=False, thresh=0.4, last_network_callback_time=0.0):
         self.debug = debug
         if test_video_picture is not None:
-            self.camera = cv2.VideoCapture(test_video_picture)    
-        elif self.debug:    
+            self.camera = cv2.VideoCapture(test_video_picture)
+        elif self.debug:
             self.camera = cv2.VideoCapture(camera)
             self.camera.set(3, args.width)
             self.camera.set(4, args.height)
@@ -30,7 +30,7 @@ class JetsonLiveObjectDetection():
         self.last_network_callback_time = last_network_callback_time
 
         # Default to not running the network node on either camera, must push an enable/disable message from statemachine
-        # We do this so we can control what states the network is actually running in 
+        # We do this so we can control what states the network is actually running in
         self.run_network_front = False
         self.run_network_bottom = False
 
@@ -48,15 +48,15 @@ class JetsonLiveObjectDetection():
         sys.exit(0)
 
     def _visualizeDetections(self, img, scores, boxes, classes, num_detections):
-        ''' Draws detections on images and returns a list of the detections name in string form 
+        ''' Draws detections on images and returns a list of the detections name in string form
 
         Args:
             img: OpenCV Mat, The current OpenCV Image
-            scores: Float List, List of confidence values 
+            scores: Float List, List of confidence values
             boxes: 2D Float List, Bounding boxes for the objects
             classes: Float List, List of the IDs for each detected object
             num_detections: Float/Int, Number of detections found
-        
+
         Returns:
             img: OpenCV Mat, Newly drawn-on image
             detections: String List, Actual names of object detected in a list
@@ -127,13 +127,13 @@ class JetsonLiveObjectDetection():
 
         print ("Starting Live object detection, may take a few minutes to initialize...")
         self.detector.initializeSession()
-        
+
         # Run the code with static video/picture testing:
         if test_video_picture is not None:
-           self.static_video() 
+           self.static_video()
 
         # Run the code in DEBUG mode. That is, with a local camera.
-        elif self.debug:  
+        elif self.debug:
             # Health Checks:
             if not self.camera.isOpened():
                 print ("Camera has failed to open")
@@ -151,7 +151,7 @@ class JetsonLiveObjectDetection():
                     scores, boxes, classes, num_detections = self.detector.detect(img)
                     new_detections = None
                     img, new_detections = self._visualizeDetections(img, scores, boxes, classes, num_detections)
-            
+
                     print ("Found objects: " + str(' '.join(new_detections)) + ".")
                     if (args.show_video):
                         cv2.imshow('Jetson Live Detection', img)
@@ -159,7 +159,7 @@ class JetsonLiveObjectDetection():
                         img_name = "{}opencv_frame_{}.jpg".format(save_dir, int(curr_time))
                         cv2.imwrite(img_name, img)
                         img_counter = 0
-                
+
                     img_counter += 1
 
                     # Publish ros-bridged images
@@ -177,7 +177,7 @@ class JetsonLiveObjectDetection():
                     print ("Network running at: " + str(1.0/(time.time() - curr_time)) + " Hz.")
 
                 frame_counter += 1
-            
+
                 if cv2.waitKey(1) == ord('q'):
                     break
 
@@ -196,12 +196,12 @@ class JetsonLiveObjectDetection():
             rospy.spin()
 
     def run_network_node_front(self, msg):
-        ''' Runs network node on the recevial of an image from ROS 
+        ''' Runs network node on the recevial of an image from ROS
 
         Args:
-            msg: ROS OpenCV Brige message, image to process on 
+            msg: ROS OpenCV Brige message, image to process on
         '''
-        
+
         # Disable the front camera check
         if not self.run_network_front:
             return
@@ -229,10 +229,10 @@ class JetsonLiveObjectDetection():
         self.last_network_callback_time = time.time()
 
     def run_network_node_bottom(self, msg):
-        ''' Runs network node on the recevial of an image from ROS 
+        ''' Runs network node on the recevial of an image from ROS
 
         Args:
-            msg: ROS OpenCV Brige message, image to process on 
+            msg: ROS OpenCV Brige message, image to process on
         '''
 
         # Disable the bottom camera check
@@ -269,14 +269,14 @@ class JetsonLiveObjectDetection():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This script runs inference on a trained object detection network")
-    parser.add_argument('-m', '--model', default="ssd_mobilenet_v1_coco", help="set name of neural network model to use")
+    parser.add_argument('-m', '--model', default="comp_22_layout", help="set name of neural network model to use")
     parser.add_argument('-v', '--verbosity', action='store_true', help="set logging verbosity (doesn't work)")
     parser.add_argument('-d', '--debug', action='store_true', help='Runs the network using a local camera, not from ROS, but will still publish to ROS topics.')
     parser.add_argument('-c', '--camera', default='/dev/camera0', help='/path/to/video, defaults to /dev/video0')
     parser.add_argument('--height', default=420, help='Set the video capture height for your camera in pixels')
     parser.add_argument('--width', default=860, help='Set the video capture width for your camera in pixels')
     parser.add_argument('-r', '--rate', type=int, default=-1, help='Specify the rate to run the neural network at, i.e. number of images to look at per second. Defaults to fastests possible.')
-    parser.add_argument('-l', '--label', default='label_map.pbtxt', help='Override the name of the label map in your model directory. Defaults to label_map.pbtxt')
+    parser.add_argument('-l', '--label', default='Badge-TommyGun-gman-bootlegger-telephone-notepad-whiskey-barrel_label_map.pbtxt', help='Override the name of the label map in your model directory. Defaults to label_map.pbtxt')
     parser.add_argument('--test-video', help='/path/to/test/video This is used if you want to test your network on a static video. It will append \'_output\' to your file before saving it.')
     parser.add_argument('--test-picture', help='/path/to/test/picture This is used if you want to test your network on a static picture. It will append \'_output\' to your file before saving it.')
     parser.add_argument('--thresh', default=0.4, help='Override the default detection threshold. Default = 0.4')
@@ -286,10 +286,10 @@ if __name__ == "__main__":
     parser.add_argument('--front-start-on', action='store_true', help='Will start with the front camera node running')
     parser.add_argument('--bottom-start-on', action='store_true', help='Will start with the bottom camera node running')
 
-    if args.no_ros:
+    if parser.parse_args().no_ros:
         args = parser.parse_args()
 
-    if not args.no_ros:
+    if not parser.parse_args().no_ros:
         import cv_bridge
         import rospy
         from sensor_msgs.msg import Image
@@ -299,10 +299,10 @@ if __name__ == "__main__":
         args = parser.parse_args(rospy.myargv()[1:])
 
         rospy.init_node('Network_Vision')
-        
+
         front_img_pub = rospy.Publisher('front_network_imgs', Image, queue_size=1)
         front_detections_pub = rospy.Publisher('front_network_output', Detections, queue_size=1)
-        
+
         bottom_img_pub = rospy.Publisher('bottom_network_imgs', Image, queue_size=1)
         bottom_detections_pub = rospy.Publisher('bottom_network_output', Detections, queue_size=1)
 
@@ -324,5 +324,5 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, live_detection.signal_handler)
 
     live_detection.start()
-    
+
 
