b531391702681a2627a607fae213b439fbcfffd3
CoryBorek <cborek@students.kennesaw.edu>
2022-07-30 16:33:02 -0400

    set depth onbce

--- src/subdriver/src/StateMachine/test_states/backwards_test_state.py
+++ src/subdriver/src/StateMachine/test_states/backwards_test_state.py
@@ -13,8 +13,9 @@ class Backwards(Sub):
 
 		msg = self.init_joy_msg()
 
+		start_depth = self.get_depth()
 		depth = self.get_depth()
-		pid = PID(1, 0.1, 0.3, setpoint=self.get_depth())
+		pid = PID(1, 0.1, 0.3, setpoint=start_depth)
 		pid.output_limits = (-0.5, 0.5)
 		
 		last_time = self.current_state_start_time
--- src/subdriver/src/StateMachine/test_states/rotate_left_test_state.py
+++ src/subdriver/src/StateMachine/test_states/rotate_left_test_state.py
@@ -16,9 +16,9 @@ class RotateLeft(Sub):
 
 		msg.axes[const.AXES['rotate']] = rotation_magnitude
 		# for 5 seconds, go rotate at whatever magnitude
-
+		start_depth = self.get_depth()
 		depth = self.get_depth()
-		pid = PID(1, 0.1, 0.3, setpoint=self.get_depth())
+		pid = PID(1, 0.1, 0.3, setpoint=start_depth)
 		pid.output_limits = (-0.5, 0.5)
 		
 		last_time = self.current_state_start_time
