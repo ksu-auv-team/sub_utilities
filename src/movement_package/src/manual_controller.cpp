c1d52da9a7da8dae48edfe23304743b7b0c15eda
Noah Trinite <trinitenoah@gmail.com>
2023-01-31 19:16:35 -0500

    remapped controls for manual controller because they got fucked and we don't know why

--- src/movement_package/src/manual_controller.cpp
+++ src/movement_package/src/manual_controller.cpp
@@ -1,11 +1,10 @@
 #include "manual_controller.h"
-
 using namespace controller;
 
 ManualController::ManualController()
 {
     _joyStickSub = _n.subscribe("joy", 10, &ManualController::JoyStickCallback, this);
-    
+
     _n.setParam("joy_node/dev", "/dev/input/js0");
 
     _joyMsg.axes = {-0.0, -0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
@@ -31,15 +30,17 @@ void ManualController::SafeArm()
 {
     int messageTime(ros::Time::now().toSec() - _lastMsgRecieved);
     if (!_manualArmed){
-        if (_joyMsg.axes[2] < -0.5 && messageTime < 60)//trigger pressed
+        auto trigger_val = _joyMsg.axes[5];
+        if (trigger_val < -0.5 && messageTime < 60)//trigger pressed
         {
             this->Arm();
             _manualArmed = true;
         }
     }
-    else //armed 
+    else //armed
     {
-    if (_joyMsg.axes[2] >= -0.5) // trigger not pressed
+    auto trigger_val = _joyMsg.axes[5];
+    if (trigger_val >= -0.5) // trigger not pressed
      {
         this->Disarm();
         _manualArmed = false;
@@ -55,8 +56,14 @@ void ManualController::SafeArm()
 void ManualController::ProcessChannels()
 {
     SafeArm();//trigger-arm
-    MavrosCommunicator->SetOverrideMessage(LATERAL_CHAN, _joyMsg.axes[3]*-500 + MID_PWM);//right stick left-right
-    MavrosCommunicator->SetOverrideMessage(FORWARD_CHAN, _joyMsg.axes[4]*500 + MID_PWM);//right stick up-down
-    MavrosCommunicator->SetOverrideMessage(THROTTLE_CHAN, _joyMsg.axes[1]*500 + MID_PWM);//left stick up-down
-    MavrosCommunicator->SetOverrideMessage(YAW_CHAN, _joyMsg.axes[0]*-500 + MID_PWM);//left stick left-right
+
+    // VALIDATE THAT THESE ARE STILL GOOD, had
+    float lateral_input  = _joyMsg.axes[2];
+    float forward_input  = _joyMsg.axes[3];
+    float throttle_input = _joyMsg.axes[1];
+    float yaw_input      = _joyMsg.axes[0];
+    MavrosCommunicator->SetOverrideMessage(LATERAL_CHAN, lateral_input*-500 + MID_PWM);//right stick left-right
+    MavrosCommunicator->SetOverrideMessage(FORWARD_CHAN, forward_input*500 + MID_PWM);//right stick up-down
+    MavrosCommunicator->SetOverrideMessage(THROTTLE_CHAN, throttle_input*500 + MID_PWM);//left stick up-down
+    MavrosCommunicator->SetOverrideMessage(YAW_CHAN,      yaw_input*-500 + MID_PWM);//left stick left-right
 }
