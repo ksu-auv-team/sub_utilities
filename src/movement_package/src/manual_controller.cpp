fb06f3c3123baf493275091a9f6760d65404dfa6
Noah Trinite <trinitenoah@gmail.com>
2023-03-09 21:45:17 -0500

    WIP: Allow for different manual mode configs

--- src/movement_package/src/manual_controller.cpp
+++ src/movement_package/src/manual_controller.cpp
@@ -1,12 +1,42 @@
 #include "manual_controller.h"
+#include <unordered_map>
+#include <string>
 
 using namespace controller;
 
 ManualController::ManualController()
 {
     _joyStickSub = _n.subscribe("joy", 10, &ManualController::JoyStickCallback, this);
-    
+
     _n.setParam("joy_node/dev", "/dev/input/js0");
+
+    // Need to find a way to allow people to be dumb and use
+    // buttons for axes and viceversa
+    // also to allow inverse for axes
+    std::unordered_map<std::string, uint8_t> axes_map = {
+        {"LLR", 0}, // Left thumbstick Left/right
+        {"LUD", 1}, // Left thumbstick Up/Down
+        {"LT", 2},  // Left trigger
+        {"RLR", 3}, // Right thumbstick Left/Right
+        {"RUD", 4}, // Right thumbstick Up/Down
+        {"RT", 5},  // Right Trigger
+        {"DP1", 6}, // D-Pad NOTE: PROBABLY DOESN'T WORK
+        {"DP2", 7}, // D-Pad NOTE: PROBABLY DOESN'T WORK
+    };
+
+    std::unordered_map<std::string, uint8_t> buttons_map = {
+        {"A", 0},
+        {"B", 1},
+        {"X", 2},
+        {"Y", 3},
+        {"LB", 4}, // Left Bumper
+        {"RB", 5}, // Right Bumper
+        {"SELECT", 6},
+        {"START", 7},
+        {"XB", 8}, // Xbox Button
+        {"L3", 9}, // Click in left stick
+        {"R3", 10}, // Click in right stick
+    };
 
     _joyMsg.axes = {-0.0, -0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     _joyMsg.buttons = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -37,7 +67,7 @@ void ManualController::SafeArm()
             _manualArmed = true;
         }
     }
-    else //armed 
+    else //armed
     {
     if (_joyMsg.axes[2] >= -0.5) // trigger not pressed
      {
@@ -59,4 +89,7 @@ void ManualController::ProcessChannels()
     MavrosCommunicator->SetOverrideMessage(FORWARD_CHAN, _joyMsg.axes[4]*500 + MID_PWM);//right stick up-down
     MavrosCommunicator->SetOverrideMessage(THROTTLE_CHAN, _joyMsg.axes[1]*500 + MID_PWM);//left stick up-down
     MavrosCommunicator->SetOverrideMessage(YAW_CHAN, _joyMsg.axes[0]*-500 + MID_PWM);//left stick left-right
+
+    //amateur hour
+    //MavrosCommunicator->SetOverrideMessage()
 }
