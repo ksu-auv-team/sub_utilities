e69a638bb97ebcf616229a44875b7640674db0c3
LoganO37 <lowens33@protonmail.com>
2023-04-06 15:03:06 -0400

    Worked on Main.py

--- src/depth_arduino/src/main.py
+++ src/depth_arduino/src/main.py
@@ -1,5 +1,23 @@
+#!/usr/bin/env python2
 import rospy
+import depth
 
-rospy.init_node("depth_arduino")
-rospy.loginfo("node depth_arduino has been started.")
-publisher = rospy.Publisher("")
+def startup():
+    pub = rospy.Publisher('depth', String, queue_size=10)#Replace String and maybe change quesize
+    rospy.init_node("depth_arduino")
+    rate = rospy.Rate(0.0166667) #60 seconds, maybe change later
+    rospy.loginfo("node depth_arduino has been started.")
+    
+    while not rospy.is_shutdown():
+        data = depth
+        depth = data.get_depth()
+        temp = data.get_temp()
+        pub.publish(depth, temp)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        startup()
+    except rospy.ROSInterruptException:
+        pass
