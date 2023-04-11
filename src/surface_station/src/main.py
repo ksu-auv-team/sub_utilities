be82809a7673dbd446221f96b4d290c66dc1d549
msle237-lees <msle237.lees@gmail.com>
2023-04-11 18:48:12 -0400

    maybe fixed the surface view

--- src/surface_station/src/main.py
+++ src/surface_station/src/main.py
@@ -1,39 +1,19 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 import rospy
 import cv2
+import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from Tkinter import Tk, Canvas, BOTH, PhotoImage, StringVar, Label
+import Tkinter as tk
+from PIL import ImageTk, Image as PilImage
+import io
 
-class App:
-    def __init__(self, master):
-        self.master = master
-        master.title("ROS Camera Viewer")
-
-        self.canvas = Canvas(master, width=640, height=480)
-        self.canvas.pack(fill=BOTH, expand=1)
-
-        self.label_text = StringVar()
-        self.label = Label(master, textvariable=self.label_text)
-        self.label.pack()
-
-        self.label_text_2 = StringVar()
-        self.label_2 = Label(master, textvariable=self.label_text_2)
-        self.label_2.pack()
-
-        self.label_text_3 = StringVar()
-        self.label_3 = Label(master, textvariable=self.label_text_3)
-        self.label_3.pack()
-
+class ROSCameraViewer:
+    def __init__(self):
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, self.image_callback)
+        self.image_sub = rospy.Subscriber("/camera/image_raw", Image, self.image_callback)
         self.current_frame = None
-
-        # self.data = rospy.Subscriber("/data", StringVar, self.grab_data)
-
-        master.after(10, self.update_image)
-        self.update_label()
 
     def image_callback(self, msg):
         try:
@@ -42,42 +22,38 @@ class App:
         except CvBridgeError as e:
             print(e)
 
-    def update_image(self):
-        if self.current_frame is not None:
-            cv2_image = cv2.cvtColor(self.current_frame, cv2.COLOR_BGR2RGBA)
-            img = cv2.resize(cv2_image, (640, 480))
-            photo = PhotoImage(image=cv2ImageTk.PhotoImage(img))
-            self.canvas.create_image(0, 0, image=photo, anchor="nw")
-            self.canvas.image = photo
-        self.master.after(10, self.update_image)
-
-    # def grab_data(self):
-    #     # Grab data from ROS
-    #     data = rospy.get_param("/data")
-    #     return data
-
-    def update_label(self):
-        # get updated data
-        # data = self.grab_data()
-        data = [0, 1, 2]
-        # parse data
-        temp = "Temperature: {}".format(data[0])
-        hum = "Humidity: {}".format(data[1])
-        depth = "Depth: {}".format(data[2])
-
-        # update labels
-        self.label_text.set(temp)
-        self.label_text_2.set(hum)
-        self.label_text_3.set(depth)
-
-        # update every second
-        self.root.after(1000, self.update_label)
+    def get_frame(self):
+        return self.current_frame
 
 def main():
-    rospy.init_node("ros_camera_viewer", anonymous=True)
-    root = Tk()
-    app = App(root)
+    rospy.init_node('ros_camera_viewer', anonymous=True)
+    viewer = ROSCameraViewer()
+
+    root = tk.Tk()
+    root.title("ROS Camera Viewer")
+
+    frame = tk.Frame(root)
+    frame.pack()
+
+    label = tk.Label(frame)
+    label.pack()
+
+    def update_image():
+        cv_img = viewer.get_frame()
+        if cv_img is not None:
+            img = PilImage.fromarray(cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB))
+            imgtk = ImageTk.PhotoImage(image=img)
+            label.imgtk = imgtk
+            label.configure(image=imgtk)
+
+        root.after(20, update_image)
+
+    def on_closing():
+        root.destroy()
+
+    update_image()
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
