bfef672d0a463b4298dda3069977dafd1f82e855
AUV@KSU <ksu.auv@gmail.com>
2023-04-11 18:33:56 -0400

    moved comments, removed unused stuff -Noah Trinite

--- src/movement_package/src/manual_controller.cpp
+++ src/movement_package/src/manual_controller.cpp
@@ -13,14 +13,22 @@ ManualController::ManualController()
     _joyMsg.buttons = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     /*
+        So according to this site: https://ardupilot.org/copter/docs/common-servo.html, the servos are indexed starting at 0 not 1.
+        Its located in the following section: Controlling the servo as a servo.
+
+        So in our code, writing to Pin 9 is actually the SERVO8_FUNCTION in QGroundControl. This might cause some problems when
+        going back to Meta Knight.
+    */
+
+    /*
         Below sets up the command that needs to be sent to the pixhawk based on user input.
     */
     _srv.request.command = 183; // command to send. should link to MAV_CMD_DO_SET_SERVO
-    __srv.request.param1 = 9; // pins on the pixhawk are 1 - indexed according to documentation
-    _srv.request.param3 = 0; 
-    _srv.request.param4 = 0; 
-    _srv.request.param5 = 0; 
-    _srv.request.param6 = 0; 
+    _srv.request.param1 = 9; // pins on the pixhawk are 1 - indexed according to documentation
+    _srv.request.param3 = 0;
+    _srv.request.param4 = 0;
+    _srv.request.param5 = 0;
+    _srv.request.param6 = 0;
     _srv.request.param7 = 0;
 
     this->Arm();
@@ -66,13 +74,7 @@ void ManualController::SafeArm()
 
 void ManualController::ProcessChannels()
 {
-    /*
-        So according to this site: https://ardupilot.org/copter/docs/common-servo.html, the servos are indexed starting at 0 not 1. 
-        Its located in the following section: Controlling the servo as a servo. 
 
-        So in our code, writing to Pin 9 is actually the SERVO8_FUNCTION in QGroundControl. This might cause some problems when
-        going back to Meta Knight.
-    */
     SafeArm();//trigger-arm
 
     /*
@@ -88,23 +90,10 @@ void ManualController::ProcessChannels()
         srv.request.param2 = 1500; // pwm
     }
 
-    /*
-        Send the command and receive the result
-    */
     bool result = _arm_srv.call(_srv);
-        
-
-    /*
-        Print the debug to the ROS terminal/log
-    */    
-    // if (result) {
-    //     ROS_INFO("Result Value %d   Command sent: %d    Pin Selected: %.0f    PWM Value Sent: %.0f", result, srv.request.command, srv.request.param1, srv.request.param2);
-    // }   
 
     MavrosCommunicator->SetOverrideMessage(LATERAL_CHAN, _joyMsg.axes[3]*-500 + MID_PWM);//right stick left-right
     MavrosCommunicator->SetOverrideMessage(FORWARD_CHAN, _joyMsg.axes[4]*500 + MID_PWM);//right stick up-down
     MavrosCommunicator->SetOverrideMessage(THROTTLE_CHAN, _joyMsg.axes[1]*500 + MID_PWM);//left stick up-down
     MavrosCommunicator->SetOverrideMessage(YAW_CHAN, _joyMsg.axes[0]*-500 + MID_PWM);//left stick left-right
-    // MavrosCommunicator->SetOverrideMessage(ARM_CHAN, 1580);//left stick left-right
-
 }
