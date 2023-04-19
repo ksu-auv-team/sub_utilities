3e811fdbd58b9c98b6bfa58ecefdbe496e42f79a
KSU AUV <ksu.auv@gmail.com>
2023-04-19 12:14:02 -0400

    added the server code into the repo - Lees

---
+++ src/camera_numpy/server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import logging
+
+from numpysocket import NumpySocket
+import cv2
+import imutils
+
+logger = logging.getLogger('OpenCV server')
+logger.setLevel(logging.INFO)
+
+with NumpySocket() as s:
+    s.bind(('192.168.1.122', 9999))
+
+    while True:
+        try:
+            s.listen()
+            conn, addr = s.accept()
+
+            logger.info(f"connected: {addr}")
+            while conn:
+                frame = conn.recv()
+                if len(frame) == 0:
+                    break
+
+                resized = imutils.resize(frame, width=1280)
+                cv2.imshow("resized", resized)
+
+                # Press Q on keyboard to exit
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    exit(1)
+            logger.info(f"disconnected: {addr}")
+        except ConnectionResetError:
+            pass
